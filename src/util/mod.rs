e84a77954cbe4a444baed050b34d2d5399477c04
BarbacoviF <f.barbacovi@nchain.com>
2025-02-05 12:47:03 +0000

    perf: Default tx (#1)
    Add function to generate default transaction based on TxVarConfig

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,5 +1,10 @@
 use byteorder::{LittleEndian, WriteBytesExt};
 use std::io::Result as IoResult;
+use chain_gang::messages::{Tx, TxIn, TxOut, OutPoint};
+use chain_gang::script::Script;
+use chain_gang::util::Hash256;
+
+use crate::constraints::tx::TxVarConfig;
 
 /// Convert u64 to var_int
 pub fn u64_to_var_int(length: usize) -> IoResult<Vec<u8>> {
@@ -18,4 +23,62 @@ pub fn u64_to_var_int(length: usize) -> IoResult<Vec<u8>> {
     }
 
     Ok(s)
+}
+
+/// Generate default Tx according to TxVarConfig
+pub fn default_tx<P: TxVarConfig>() -> Tx {
+    let version: u32 = 0;
+    let mut inputs: Vec<TxIn> = Vec::with_capacity(P::N_INPUTS);
+    for i in 0..P::N_INPUTS {
+        inputs.push(TxIn {
+            prev_output: OutPoint {
+                hash: Hash256([0; 32]),
+                index: 0,
+            },
+            unlock_script: Script(vec![0; P::LEN_UNLOCK_SCRIPTS[i]]),
+            sequence: 0,
+        })
+    }
+    let mut outputs: Vec<TxOut> = Vec::with_capacity(P::N_OUTPUTS);
+    for i in 0..P::N_OUTPUTS {
+        outputs.push(TxOut {
+            satoshis: 0,
+            lock_script: Script(vec![0; P::LEN_LOCK_SCRIPTS[i]]),
+        })
+    }
+    let lock_time: u32 = 0;
+    Tx {
+        version,
+        inputs,
+        outputs,
+        lock_time,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::constraints::tx::TxVarConfig;
+
+    use super::default_tx;
+
+
+    #[derive(Clone)]
+    struct Config;
+    impl TxVarConfig for Config {
+        const N_INPUTS: usize = 1;
+        const N_OUTPUTS: usize = 1;
+        const LEN_UNLOCK_SCRIPTS: &[usize] = &[0x6b];
+        const LEN_LOCK_SCRIPTS: &[usize] = &[0x19];
+        const LEN_PREV_LOCK_SCRIPT: Option<usize> = None;
+        const PRE_SIGHASH_N_INPUT: Option<usize> = None;
+    }
+    
+    #[test]
+    fn test_default_tx() {
+        let test_tx = default_tx::<Config>();
+        assert_eq!(test_tx.inputs.len(), 1);
+        assert_eq!(test_tx.outputs.len(), 1);
+        assert_eq!(test_tx.inputs[0].unlock_script.0.len(), 0x6b);
+        assert_eq!(test_tx.outputs[0].lock_script.0.len(), 0x19);
+    }
 }
