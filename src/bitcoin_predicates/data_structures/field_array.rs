daad34fdeb1f9f435b3901d6b82d073a686ca6fa
Federico Barbacovi <f.barbacovi@teranode.group>
2025-03-17 10:24:26 +0000

    Fix typo

--- src/bitcoin_predicates/data_structures/field_array.rs
+++ src/bitcoin_predicates/data_structures/field_array.rs
@@ -1,4 +1,4 @@
-//! Implement `FieldVector`, to be used as a variable in Bitcoin Predicates
+//! Implement `FieldArray`, to be used as a variable in Bitcoin Predicates
 use std::borrow::Borrow;
 use std::marker::PhantomData;
 
